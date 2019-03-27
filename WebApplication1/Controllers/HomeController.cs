eefbf6d0abbdbe39df0ccc014cd9e500ebabfb73
luoxi <fluoxi@qq.com>
2019-03-27 14:51:03 +0800

    添加项目文件。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.path = "/pdfjs-2.0.943-dist/web/compressed.tracemonkey-pldi-09.pdf";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
